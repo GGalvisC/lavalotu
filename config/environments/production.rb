228ec34bd26f0ab9d91840c91753eee76ea1303a
ggalvisc <gagalvisc@unal.edu.co>
2016-09-16 01:26:41 -0500

    fix production7

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,8 +12,8 @@ Rails.application.configure do
     :address    => 'smtp.sendgrid.net',
     :port       => '587',
     :authentication => :plain,
-    :user_name      => 'app56670026@heroku.com',#ENV['SENDGRID_USERNAME'],
-    :password       => 'llepu5ro4420',#ENV['SENDGRID_PASSWORD'],
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
